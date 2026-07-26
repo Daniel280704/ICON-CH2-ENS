82ff6b8e5624b94758d769079607e9a19cd01441
Daniel <154078145+Daniel280704@users.noreply.github.com>
2026-07-26 10:17:08 +0200

    Update icon_ch2_hourly_snow_probability.py

--- icon_ch2_hourly_snow_probability.py
+++ icon_ch2_hourly_snow_probability.py
@@ -275,7 +275,7 @@ def genera_album_orari(dt_run_utc: datetime, nome_run: str):
             
             plt.close(chart.fig)
         
-        caption_album = f"❄️ ICON-CH2 EPS: Probabilità Neve oraria ≥ 0.5 mm eq. acqua\n{block_name}\nRun {nome_run}"
+        caption_album = f"ICON-CH2 EPS: Probabilità Neve oraria ≥ 0.5 mm eq. acqua\n{block_name}\nRun {nome_run}"
         invia_album_telegram(percorsi_foto, caption_album)
         
         for f in percorsi_foto:
